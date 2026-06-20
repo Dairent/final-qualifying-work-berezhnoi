1261e77e540382f4390f4d07469a32a15d0389d7
Dairent <82454060+Dairent@users.noreply.github.com>
2026-06-20 11:27:35 +0300

    Create StatusEffect.gd

---
+++ scripts/entities/StatusEffect.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name StatusEffect
+
+export(String) var effect_name = "Эффект"
+export(int) var duration = 3
+export(bool) var is_debuff = true
+export(String) var description = "Описание эффекта"
