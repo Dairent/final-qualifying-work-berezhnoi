84bb0cb42f07cee658eceb0b1e59754dc3fc333c
Dairent <82454060+Dairent@users.noreply.github.com>
2026-06-20 11:27:45 +0300

    Create HeroClassData.gd

---
+++ scripts/entities/HeroClassData.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+export(String) var class_name = "Герой"
+export(int) var max_hp = 100
+export(int) var attack = 15
+export(int) var defense = 5
+export(int) var move_points = 4
+export(int) var initiative = 10
+export(Array, String) var skills = []
+export(Texture) var portrait = null
+export(String) var description = "Описание класса"
