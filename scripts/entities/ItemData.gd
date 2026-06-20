14e73f09d46e2f721672ab8d7c1e6d428e7e03e0
Dairent <82454060+Dairent@users.noreply.github.com>
2026-06-20 11:27:55 +0300

    Create ItemData.gd

---
+++ scripts/entities/ItemData.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name ItemData
+
+export(String) var item_name = "Предмет"
+export(String) var description = "Описание"
+export(int) var cost = 10
+export(Texture) var icon = null
+export(String) var effect_type = "heal"
+export(int) var effect_value = 0
